4ae13c322cfd57288d5299bc1526da6872776fc6
unknown <vincentjabonete10@gmail.com>
2020-05-31 12:18:09 +0800

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router'; // to navigate the directive components
+import { MatButtonModule } from '@angular/material/button'; // to style the button 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    RouterModule.forRoot ([
+      {
+        path: 'home',
+        component:HomeComponent
+      }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
